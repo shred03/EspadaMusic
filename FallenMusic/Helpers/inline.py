d78fab8f0b8c0c16d33fee6e836273dc7c61764e
shred03 <mithunyada12345@gmail.com>
2025-08-03 17:10:53 +0530

    added my repo

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -56,7 +56,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/AnonymousX1025/FallenMusic"
+            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/shred03/EspadaMusic"
         ),
         InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
     ],
